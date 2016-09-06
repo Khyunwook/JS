54f7f8154e77524185f3c01e9bfc4256b7ffe835
Khyunwook <khwbori@gmail.com>
2016-09-07 03:02:50 +0900

    Update The_Array_forEach_method.js

--- The_Array_forEach_method.js
+++ The_Array_forEach_method.js
@@ -10,10 +10,27 @@ function getStockSymbols( stocks ){
   return symbols;
 }
 
+function getStockSymbols2( stocks ){
+  var symbols = [];
+  stocks.forEach(function(stock){
+     symbols.push(stock.symbol);
+  });
+  return symbols;
+}
+
 var symbols = getStockSymbols([
   { symbol: "XFX", price: 240.22, volume: 23432 },
   { symbol: "TNZ", price: 332.19, volume: 234 },
   { symbol: "JXJ", price: 120.22, volume: 5323 },
 ]);
 
-console.log(JSON.stringify(symbols));
+var symbols2 = getStockSymbols2([
+  { symbol: "XFX", price: 240.22, volume: 23432 },
+  { symbol: "TNZ", price: 332.19, volume: 234 },
+  { symbol: "JXJ", price: 120.22, volume: 5323 },
+]);
+
+console.log(symbols); 
+//["XFX", "TNZ", "JXJ"]
+console.log(symbols2);
+//["XFX", "TNZ", "JXJ"]
