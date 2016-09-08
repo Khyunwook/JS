d95e25b4a041c88f0659f3d95f7cf564b308521d
Khyunwook <khwbori@gmail.com>
2016-09-08 15:01:45 +0900

    Update Create_an_Array_concatAll_method.js

--- Create_an_Array_concatAll_method.js
+++ Create_an_Array_concatAll_method.js
@@ -27,3 +27,7 @@ var stocks = exchanges.concatAll();
 stocks.forEach(function(stock){
   console.log(JSON.stringify(stock));
 });
+//"{\"symbol\":\"XFX\",\"price\":240.22,\"volume\":23432}"
+//"{\"symbol\":\"TNZ\",\"price\":332.19,\"volume\":234}"
+//"{\"symbol\":\"JXJ\",\"price\":120.22,\"volume\":5323}"
+//"{\"symbol\":\"NYN\",\"price\":88.47,\"volume\":98275}"
